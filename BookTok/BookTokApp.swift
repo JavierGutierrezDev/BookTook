6ed4bbf0e74e77b7c3b9f74e25456190c03e50ab
JavierGutierrezDev <gutierrezgutierrezjavier30@gmail.com>
2023-10-30 23:31:23 +0100

    Initial Commit

---
+++ BookTok/BookTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTokApp.swift
+//  BookTok
+//
+//  Created by Javier Gutierrez on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BookTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
